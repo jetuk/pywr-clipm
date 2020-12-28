05c7712679d4f3a56828f99503832fc8b4add508
James Tomlinson <tomo.bbe@gmail.com>
2020-12-28 22:28:12 +0000

    Add build option for cl-fast-relaxed-math.

--- cgpycl/cl.py
+++ cgpycl/cl.py
@@ -4,9 +4,13 @@ import os
 CL_DIR = os.path.join(os.path.dirname(__file__), 'cl')
 
 
-def get_cl_program(cl_context: cl.Context) -> cl.Program:
+def get_cl_program(cl_context: cl.Context, fast_relaxed_math: bool = False) -> cl.Program:
+
+    options = []
+    if fast_relaxed_math:
+        options.append('-cl-fast-relaxed-math')
 
     with open(os.path.join(CL_DIR, 'path_following.cl')) as fh:
-        prg = cl.Program(cl_context, fh.read()).build()
+        prg = cl.Program(cl_context, fh.read()).build(options)
 
     return prg
